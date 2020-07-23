5e7e6730e37fc1e442d5f401963d50261179c57b
Kevin <ATOMktn@protonmail.com>
2020-07-23 09:52:18 -0400

    iframe refresh

--- copout/static/scripts/iframe.js
+++
@@ -1,6 +0,0 @@
-const iframe =  document.querySelector('iframe');
-
-const refreshFrame = () => iframe.src = 'copout/static/map/map.html';
-
-window.addEventListener('load', refreshFrame);
-window.removeEventListener('load', refreshFrame);
