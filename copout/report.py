3a4c411f573571ffde90c11d4d85f2e4e5fe13fb
Kevin <ATOMktn@protonmail.com>
2020-07-27 20:30:24 -0400

    structure and styling for map tooltip report.py

--- copout/report.py
+++ copout/report.py
@@ -1,4 +1,6 @@
 import folium
+from folium.map import Popup
+
 import os
 
 import requests
@@ -26,8 +28,15 @@ def index():
         lat = report['lat']
         lng = report['lng']
         tooltip = report['city_of_inc'] + ', ' + report['state_of_inc']
-        #TODO: popup
-        folium.Marker([lat, lng], tooltip=tooltip).add_to(folium_map)
+        popup = '''
+                <aside class="map popup">
+                    <header class="header popup">
+                       <h3>''' + report['org'] + '''</h3>
+                    </header>
+                    <p class="content popup">''' + report['summary'] + '''</p>
+                </aside>
+                '''
+        folium.Marker([lat, lng], tooltip=tooltip, popup=Popup(popup, max_width=250)).add_to(folium_map)
     
     return render_template('report/index.html', reports=reports, map=folium_map.get_root().render())
 
